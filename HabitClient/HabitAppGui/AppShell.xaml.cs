26af892120af37c78d4394d7865a222924c9d512
Jaibir Singh Sandhu <jaibir.s.sandhu@gmail.com>
2023-12-20 13:53:46 -0700

    Set up MAUI

---
+++ HabitClient/HabitAppGui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HabitAppGui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HabitClient/HabitAppGui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace HabitAppGui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
