3ad4fa8becd055bcc60d4442e4ba09d352d2abb1
Jaibir Singh Sandhu <jaibir.s.sandhu@gmail.com>
2024-01-02 18:04:43 -0700

    Fleshed out the habit capture mopup, but no functionality yet

--- HabitClient/View/HabitCapture.xaml.cs
+++ HabitClient/View/HabitCapture.xaml.cs
@@ -1,3 +1,5 @@
+using Mopups.Services;
+
 namespace View;
 
 public partial class HabitCapture
@@ -5,5 +7,13 @@ public partial class HabitCapture
 	public HabitCapture()
 	{
 		InitializeComponent();
+	}
+	public void HabitCaptureConfirmClicked(object sender, EventArgs e)
+	{
+		MopupService.Instance.PopAsync();
+	}
+	public void DataCaptureCancelClicked(object sender, EventArgs e) 
+	{
+		MopupService.Instance.PopAsync();
 	}
 }
